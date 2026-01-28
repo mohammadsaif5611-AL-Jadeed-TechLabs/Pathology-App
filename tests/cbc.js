77d3a50f1007b8d4104b8906005325191336ad0e
mohammadsaif5611-AL-Jadeed-TechLabs <mohammadsaif5611@gmail.com>
2026-01-28 11:17:45 +0530

    Update cbc.js

--- tests/cbc.js
+++ tests/cbc.js
@@ -276,7 +276,7 @@ y += 7; // row spacing
   // ================= CBC DATA (NO ROW REMOVED) =================
   row("HAEMOGLOBIN", data.Haemoglobin, "gm %", ["F: 11-16", "M: 14-18"], data.Sex);
   
-  row("TOTAL LEUCOCYTE COUNT", data.TLC, "/cumm", ["4,000 - 10,000"], data.Sex);
+  row("TOTAL LEUCOCYTE COUNT", data.TLC, "/cumm", ["4000-10000"], data.Sex);
 
   pdf.text("DIFF. LEUCOCYTE COUNT", 20, y);
   y += 6;
@@ -298,7 +298,9 @@ row("BASOPHILS", data.Basophils, "%", ["0 - 1"], data.Sex, 10);
   row("MCHC", data.MCHC, "g/dl", ["31 - 36"], data.Sex);
   row("RDW-CV", data.RDW_CV, "%", ["10 - 15"], data.Sex);
   row("RDW-SD", data.RDW_SD, "fl", ["39 - 46"], data.Sex);
-  row("PLATELET COUNT", data.Platelet, "/cumm", ["1,50,000 - 4,50,000"], data.Sex);
+
+row("PLATELET COUNT", data.Platelet, "/cumm", ["150000-450000"], data.Sex);
+
   row("MPV", data.MPV, "fl", ["6.5 - 11"], data.Sex);
   row("PCT", data.PCT, " ", ["0.100 - 0.280"], data.Sex);
   row("PDW", data.PDW, "%", ["9.0 - 17.0"], data.Sex);
